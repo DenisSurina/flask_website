22e085aef68f76643e5f4496622555587b04c872
Denis Surina <denis.surina1@gmail.com>
2022-09-05 22:42:45 +0200

    added login page

--- flask_blog.py
+++ flask_blog.py
@@ -1,22 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 
+app.config["SECRET_KEY"] = "eec89c0e52dcd5615173ef16fc06e1c2"
 
-posts=[
 
-    {
-        'author': 'tyrion lannister',
-        'title': 'blog post'
-    },
-
-    {
-        'author': 'dennis',
-        'title': 'second breakfast'
-    }
-
+posts = [
+    {"author": "tyrion lannister", "title": "blog post"},
+    {"author": "dennis", "title": "second breakfast"},
 ]
 
 
@@ -29,6 +22,24 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", posts=posts)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+
+    form = LoginForm()()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -16,4 +16,15 @@ class RegistrationForm(FlaskForm):
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
 
-    submit = SubmitField('Sign up')
+    submit = SubmitField("Sign up")
+
+
+class LoginForm(FlaskForm):
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    remember = BooleanField("Remember me")
+
+    submit = SubmitField("Login")
